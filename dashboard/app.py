e33d7fdc45162fe25d761ea44cc28139f93b7bda
Arx Cruz <acruz@redhat.com>
2015-03-18 15:59:11 +0100

    Altering the way init_db is called

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,36 +1,44 @@
+import os.path
+
 from flask import Flask
+from oslo.config import cfg
 
 from api.rest_api import rest_api
+import config
 from dashboard import dashboard
+from gerritreport.gerrit_reports import gerrit_report
 from rhbzreports.rest_api import report_rest_api
 from rhbzreports.redhat_bugzilla_report import redhat_bugzilla_report
-
 import filters
-
-import os.path
 
 
 def create_app():
-    if not os.path.isfile('dashboard.db'):
-        from database import init_db
-        init_db()
+    from database import init_db
+    init_db(not os.path.isfile('dashboard.db'))
 
     app = Flask(__name__)
-    app.config['DEBUG'] = True
+
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stackquery.db'
     app.secret_key = 'why would I tell you my secret key?'
 
-    # db.init_app(app)
     filters.init_app(app)
     app.register_blueprint(dashboard)
     app.register_blueprint(rest_api)
     app.register_blueprint(report_rest_api)
     app.register_blueprint(redhat_bugzilla_report)
+    app.register_blueprint(gerrit_report)
 
+    conf = cfg.CONF
+    conf.register_opts(config.OPTS)
+
+    conf_file = os.getenv('STACKQUERY_CONF')
+    if conf_file and os.path.isfile(conf_file):
+        conf(default_config_files=[conf_file])
+
+    app.config['DEBUG'] = cfg.CONF.debug
     return app
 
 app = create_app()
 
 if __name__ == '__main__':
-
-    app.run()
+    app.run(cfg.CONF.listen_host, cfg.CONF.listen_port)

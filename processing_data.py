a57cb5529a75f3ead95dadc2f70496e86bb48035
Arx Cruz <acruz@redhat.com>
2015-04-17 10:36:34 +0200

    Renaming runtime.py to processing_data.py and moving his location

--- processing_data.py
+++ processing_data.py
@@ -1,15 +1,8 @@
 from stackquery.common import gerrit
-from stackquery.dashboard import config
-from oslo.config import cfg
-
-import logging
 
 
 def main():
-    conf = cfg.CONF
-    conf.register_opts(config.OPTS)
     projects = ['openstack/nova', 'stackforge/rally', 'openstack/tempest']
-    logging.basicConfig(level='DEBUG')
     for project in projects:
         gerrit.process_reviews(project)
 

b48d16f8bcf09a3374b4a66a1d41c8fd723a9e23
Arx Cruz <acruz@redhat.com>
2015-07-13 16:34:49 +0200

    Adding teardown function to remove db_session
    This isn't necessary when you're using sqlite, however, if you start
    to use mysql, it's required in order to commit and flush everything
    in database

--- stackquery/__init__.py
+++ stackquery/__init__.py
@@ -10,6 +10,13 @@ app.secret_key = 'why would I tell you my secret key?'
 
 from stackquery.views import rest_api, dashboard
 
+
+@app.teardown_request
+def remove_db_session(exception):
+    db_session.remove()
+
+from stackquery.database import db_session
+
 app.register_blueprint(dashboard.mod)
 app.register_blueprint(rest_api.mod)
 

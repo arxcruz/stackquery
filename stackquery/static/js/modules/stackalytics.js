709dd1bc0146914019500a6d9c810220d6a035c0
Arx Cruz <acruz@redhat.com>
2015-09-08 09:52:02 +0200

    Two changes: * Loading local javascript libraries instead from cdn * adding AngularJS directives

---
+++ stackquery/static/js/modules/stackalytics.js
@@ -0,0 +1,127 @@
+angular.module('stackalitics', ['angularUtils.directives.dirPagination'])
+    .factory('stackApi', stackApi)
+    .constant('stackApiUrl', 'http://stackalytics.com/api/1.0/stats/engineers')
+    .controller('stackCtrl', StackCtrl);
+
+
+function stackApi($http, restApi, stackApiUrl) {
+    return {
+        getUsersByCompany(company) {
+            return restApi.get('?company=' + company, stackApiUrl);
+        }
+    }
+}
+
+function StackCtrl($scope, stackApi, userApi) {
+
+    $scope.users = [];
+    $scope.selected = [];
+    $scope.errorMessage = '';
+    $scope.successMessage = '';
+    $scope.selectAll = false;
+    $scope.currentPage = 1;
+    $scope.pageSize = 10;
+    $scope.loading = false;
+
+    $scope.getUsers = getUsers;
+    $scope.toggleSelection = toggleSelection;
+    $scope.search = search;
+    $scope.hasError = hasError;
+    $scope.hasSuccess = hasSuccess;
+    $scope.selectAll = selectAll;
+    $scope.addUser = addUser;
+    $scope.addSelectedUsers = addSelectedUsers;
+
+    function getUsers(company) {
+        $scope.loading = true;
+        stackApi.getUsersByCompany(company)
+            .success(
+                function(data) {
+                    $scope.users = data.stats;
+                })
+            .error(
+                function(errorInfo, status) {
+                    $scope.users = [];
+                    $scope.errorMessage = 'An error occurred: ' + errorInfo.message;
+                })
+            .finally(function() {
+                $scope.loading = false;
+            });
+    }
+
+    function toggleSelection(user) {
+        var idx = $scope.selected.indexOf(user);
+        if(idx > -1) {
+            $scope.selected.splice(idx, 1);
+        } else {
+            $scope.selected.push(user);
+        }
+    }
+
+    function search() {
+        getUsers($scope.company);
+    }
+
+    function hasError() {
+        return $scope.errorMessage != '';
+    }
+
+    function hasSuccess() {
+        return $scope.successMessage != '';
+    }
+
+    function selectAll() {
+        if($scope.selected.length == $scope.users.length) {
+            $scope.selected = [];
+        } else {
+            $scope.selected = [];
+            for(var i = 0; i < $scope.users.length; i++) {
+                var user = $scope.users[i];
+                $scope.selected.push(user);
+            }
+        }
+    }
+
+    function addUser(user) {
+        return addUserBackend(user);
+        //dismissMessages();
+    }
+
+    function addSelectedUsers() {
+        for(var i = 0; i < $scope.selected.length; i++) {
+            var user = $scope.selected[i];
+            addUserBackend(user);
+        }
+    }
+
+    function addUserBackend(user) {
+        userApi.addUser({
+            name: user.name,
+            user_id: user.id
+        })
+        .success(
+            function(data) {
+                $scope.successMessage = 'User ' + user.name + ' added successfully';
+                var idx = $scope.users.indexOf(user);
+                if(idx > -1) {
+                    $scope.users.splice(idx, 1);
+                }
+                idx = $scope.selected.indexOf(user);
+                if(idx > -1) {
+                    $scope.selected.splice(idx, 1);
+                }
+            })
+        .error(
+            function(errorInfo, status) {
+                $scope.errorMessage = 'An error ocurred: ' + errorInfo.message;
+            });
+    }
+
+    function dismissMessages() {
+        setTimeout(function() {
+            $scope.successMessage = '';
+            $scope.errorMessage = '';
+            $scope.$apply();
+        }, 4000);
+    }    
+}
---
+++ stackquery/static/js/modules/users.js
@@ -0,0 +1,256 @@
+angular.module('users', [])
+    .controller('userCtrl', UserCtrl)
+    .controller('bulkUserCtrl', BulkUserCtrl)
+    .controller('userListCtrl', UserListCtrl)
+    .factory('userApi', userApi)
+    .constant('userApiUrl', '/api/v1.0/users');
+
+function userApi($http, restApi, userApiUrl) {
+    return {
+        addUser: function(user) {
+            return restApi.post(user, userApiUrl);
+        },
+        getUsers: function() {
+            return restApi.get(null, userApiUrl);
+        },
+        updateUser: function(user) {
+            return restApi.put(user, user.url);
+        },
+        delUser: function(user) {
+            return restApi.del(null, user.url);
+        }
+    }
+}
+
+function UserCtrl($scope, userApi) {
+
+    $scope.errorMessage = '';
+    $scope.successMessage = '';
+    $scope.model = {};
+    $scope.createUser = createUser;
+    $scope.hasError = hasError;
+    $scope.hasSuccess = hasSuccess
+
+    reset();
+
+    function createUser() {
+        $scope.$broadcast('show-errors-check-validity');
+
+        if ($scope.userForm.$invalid) { 
+            return; 
+        }
+
+        var user = {
+            name: $scope.model.userName,
+            user_id: $scope.model.userId,
+            email: $scope.model.userEmail
+        };
+
+        userApi.addUser(user)
+            .success(
+                function (data) {
+                    reset();
+                    $scope.successMessage = 'User added successfully';
+                    dismissMessages();
+                })
+            .error(
+                function (errorInfo, status) {
+                    $scope.errorMessage = 'An error ocurred: ' + errorInfo.message;
+                });        
+    }
+
+    function hasError() {
+        return $scope.errorMessage != '';
+    }
+
+    function hasSuccess() {
+        return $scope.successMessage != '';
+    }
+
+    function reset() {
+        $scope.model = {};
+    }
+
+    function dismissMessages() {
+        setTimeout(function() {
+            $scope.successMessage = '';
+            $scope.errorMessage = '';
+            $scope.$apply();
+        }, 4000);
+    }
+}
+
+function BulkUserCtrl($scope, userApi) {
+
+    $scope.users = [];
+    $scope.currentPage = 1;
+    $scope.pageSize = 10;
+    $scope.errorMessage = '';
+    $scope.successMessage = '';
+
+    $scope.loadUsersFromText = loadUsersFromText;
+    $scope.reset = reset;
+    $scope.addBulkUsers = addBulkUsers;
+    $scope.hasError = hasError;
+    $scope.hasSuccess = hasSuccess;
+
+    function loadUsersFromText() {
+        var usersSplited = $scope.usersText.trim().split(";");
+        for(var i = 0; i < usersSplited.length; i++) {
+            var singleUser = usersSplited[i].split(",");
+            var user = {
+                name: singleUser[0],
+                user_id: singleUser[1],
+                email: singleUser[2]
+            };
+            $scope.users.push(user);
+        }
+    }
+
+    function reset() {
+        $scope.users = [];
+        //$scope.usersText = '';
+    }
+
+    function addBulkUsers() {
+        console.log($scope.users.length);
+        for(var i = 0; i < $scope.users.length; i++) {
+            var user = $scope.users[i];
+            if(user) {
+                addUserBackend(user);    
+            }
+        }
+        $scope.users = [];
+    }
+
+    function hasError() {
+        return $scope.errorMessage != '';
+    }
+
+    function hasSuccess() {
+        return $scope.successMessage != '';
+    }
+
+    function addUserBackend(user) {
+        userApi.addUser(user)
+        .success(
+            function(data) {
+                $scope.successMessage = 'User ' + user.name + ' added successfully';
+            })
+        .error(
+            function(errorInfo, status) {
+                $scope.errorMessage = 'An error ocurred: ' + errorInfo.message;
+            });
+    }
+}
+
+function UserListCtrl($scope, userApi) {
+    var editFlag = false;
+
+    $scope.users = [];
+    $scope.selectedId = -1;
+    $scope.currentPage = 1;
+    $scope.pageSize = 10;
+    $scope.loading = false;
+    $scope.errorMessage = '';
+    $scope.successMessage = '';
+
+    $scope.refresh = refresh;
+    $scope.isInReadMode = isInReadMode;
+    $scope.isInEditMode = isInEditMode;
+    $scope.startEdit = startEdit;
+    $scope.save = save;
+    $scope.cancel = reset;
+    $scope.removeUser = removeUser;
+    $scope.hasSuccess = hasSuccess;
+    $scope.hasError = hasError;
+
+    refresh();
+
+    function refresh() {
+        $scope.loading = true;
+        $scope.users = [];
+        userApi.getUsers()
+            .success(
+                function(data) {
+                    $scope.users = data;
+                })
+            .error(
+                function(errorInfo, status) {
+
+                })
+            .finally(function() {
+                $scope.loading = false;
+                reset();
+            });
+    }
+
+    function isInReadMode(id) {
+        return selectedId < 0 || selectedId != id;
+    }
+
+    function isInEditMode(id) {
+        return selectedId == id && editFlag;
+    }
+
+    function startEdit(id) {
+        reset();
+        selectedId = id;
+        editFlag = true;
+        loading = false;
+    }
+
+    function save(user) {
+        userApi.updateUser(user)
+            .success(function(data) {
+                $scope.successMessage = 'User: ' + user.name + ' updated successfully';
+                dismissMessages();
+            })
+            .error(function(errorInfo, status) {
+                $scope.errorMessage = 'An error ocurred: ' + errorInfo.message;
+            })
+            .finally(function() {
+                reset();
+            });
+    }
+
+    function reset() {
+        selectedId = -1;
+        editFlag = false;
+    }
+
+    function removeUser(user) {
+        userApi.delUser(user)
+            .success(function(data) {
+                var idx = $scope.users.indexOf(user);
+                if(idx < 0) {
+                    return;
+                }
+                $scope.users.splice(idx, 1);
+                $scope.successMessage = 'User ' + user.name + ' deleted successfully';
+                dismissMessages();
+            })
+            .error(function(errorInfo, status) {
+                $scope.errorMessage = 'An error ocurred: ' + errorInfo.message;
+            })
+            .finally(function() {
+                reset();
+            })
+    }
+
+    function hasError() {
+        return $scope.errorMessage != '';
+    }
+
+    function hasSuccess() {
+        return $scope.successMessage != '';
+    }
+
+    function dismissMessages() {
+        setTimeout(function() {
+            $scope.successMessage = '';
+            $scope.errorMessage = '';
+            $scope.$apply();
+        }, 4000);
+    }
+}

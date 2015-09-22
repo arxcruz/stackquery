74e0d616c16b7876b1da43c168d9a11f06cade85
Arx Cruz <acruz@redhat.com>
2015-09-22 12:21:43 +0200

    Removing all unecessary files

--- stackquery/forms/project.py
+++
@@ -1,6 +0,0 @@
-from wtforms import Form, TextField, validators
-
-
-class ProjectForm(Form):
-    name = TextField('Name', [validators.Length(min=3, max=128)])
-    git_url = TextField('Git url', [validators.Length(min=4, max=255)])

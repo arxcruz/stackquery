5656237b6efe4399ac90ed6cbdcbd428b743d8d0
Arx Cruz <acruz@redhat.com>
2015-10-20 11:35:48 +0200

    Adding support to debug

--- processing_data.py
+++ processing_data.py
@@ -2,10 +2,23 @@ import stackquery.libs.gerrit as gerrit
 import stackquery.libs.utils as utils
 
 import logging
+import argparse
 
 LOG = logging.getLogger(__name__)
 
+
+def init_argparse():
+    parser = argparse.ArgumentParser(description='Processing gerrit changes')
+    parser.add_argument('--debug', action='store_true',
+                        help='Show debug message', default=False)
+    return parser.parse_args()
+
+
 def main():
+    args = init_argparse()
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+
     projects = utils.get_projects_being_used()
     LOG.debug('Projects being used: %s' % projects)
     for project in projects:
